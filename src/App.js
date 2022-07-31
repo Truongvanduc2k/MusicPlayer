2dd0623aa6a49401626d0b993094c54a97d495fe
Duc Truong <truongvanduc.81gl@gmail.com>
2022-07-31 21:13:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import PlayMusic from './Component/MusicApp';
-import SongItem from './Component/MusicApp/SongItem';
-import Productdetails from './Productdetails';
 
 function App() {
   return (
     <div className="App">
       <PlayMusic></PlayMusic>
-      {/* <Productdetails/> */}
     </div>
   );
 }
