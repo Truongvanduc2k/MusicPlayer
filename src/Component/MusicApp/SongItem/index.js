e95f8b4b251297cc8bafacbeaf89e3ac8c65bf09
Duc Truong <truongvanduc.81gl@gmail.com>
2022-08-10 22:36:02 +0700

    update

--- src/Component/MusicApp/SongItem/index.js
+++ src/Component/MusicApp/SongItem/index.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import styles from './SongItem.module.css';
-import { TiHeartOutline} from "react-icons/ti";
+import { TiHeartFullOutline } from "react-icons/ti";
 
 function SongItem(
-    { 
-      song,
-      songplaying,
-      isLike,
-      clickLike,
-      clickSongItem,
-    }) {
+  {
+    song,
+    songplaying,
+    isLike,
+    clickLike,
+    clickSongItem,
+  }) {
   return (
     <div className={[styles.container, songplaying === song.id ? styles.songPlaying : ''].join(' ')}>
-        <div className={styles.id}><h4>{song.id}</h4></div>
-        <div className={styles.imageUrl}><img src={song.imageUrl} alt='' /></div>
-        <div className={styles.title} onClick={clickSongItem}>{song.title}</div>
-        <div className={styles.artist}>{song.artist}</div>
-        <div className={styles.duration}>{`${Math.floor(song.duration / 60)}:${song.duration % 60}s`}</div>
-        <div className={songplaying === song.id ? styles.tym2 : styles.tym}
+      <div className={styles.id}><h4>{song.id}</h4></div>
+      <div className={styles.imageUrl}><img src={song.imageUrl} alt='' /></div>
+      <div className={styles.title} onClick={clickSongItem}>{song.title}</div>
+      <div className={styles.artist}>{song.artist}</div>
+      <div className={styles.duration}>{`${Math.floor(song.duration / 60)}:${song.duration % 60}s`}</div>
+      <div className={songplaying === song.id ? styles.tym2 : styles.tym}
         style={{ color: isLike === 'true' ? 'red' : '#abb6b6' }}
-        onClick={clickLike}><button><TiHeartOutline/></button></div>
+        onClick={clickLike}><button><TiHeartFullOutline /></button></div>
     </div>
   )
 }
