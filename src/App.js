726f62c09aeffeea19f287432383573426932bd0
Duc Truong <truongvanduc.81gl@gmail.com>
2022-08-11 20:16:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import PlayMusic from './Component/MusicApp';
-
+import Sessions from './Component/Sessions';
+import Productdetails from './Productdetails';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <PlayMusic></PlayMusic>
+      {/* <Sessions /> */}
+      {/* <Productdetails /> */}
     </div>
   );
 }
